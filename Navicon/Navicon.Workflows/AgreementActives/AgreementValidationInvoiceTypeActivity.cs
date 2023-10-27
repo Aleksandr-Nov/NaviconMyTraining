65f22a7e3807f4ffd8c62db725b8fcbdd68831ac
Александр Новиков <sasha4866@gmail.com>
2023-10-27 22:09:54 +0300

    Task-6

---
+++ Navicon/Navicon.Workflows/AgreementActives/AgreementValidationInvoiceTypeActivity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using Navicon.Workflows.AgreementActives.Handlers;
+using System;
+using System.Activities;
+
+namespace Navicon.Workflows.AgreementActives
+{
+    public class AgreementValidationInvoiceTypeActivity : BaseWorkflow
+    {
+        [Input("nav_agreement")]
+        [RequiredArgument]
+        [ReferenceTarget("nav_agreement")]
+        public InArgument<EntityReference> AgreementReference { get; set; }
+
+        [Output("Invoice status")]
+        public OutArgument<bool> IsValid { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            try
+            {
+                var agreementRef = AgreementReference.Get(context);
+                AgreementService agreementService = new AgreementService(GetService(context), agreementRef);
+                if (agreementRef == null || agreementRef.Id == null)
+                {
+                    throw new ArgumentNullException(nameof(agreementRef), "Agreement not found");
+                }
+
+                IsValid.Set(context, agreementService.CheckLinkedInvoice(false, true));
+            }
+            catch (Exception exc)
+            {
+                throw new Exception(exc.Message);
+            }
+
+        }
+    }
+}
---
+++ Navicon/Navicon.Workflows/BaseWorkflow.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navicon.Workflows
+{
+    public class BaseWorkflow : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+        public IOrganizationService GetService(CodeActivityContext context)
+        {
+            var serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            var service = serviceFactory.CreateOrganizationService(Guid.Empty);
+            if (service != null && service is IOrganizationService)
+            {
+                return service;
+            }
+
+            throw new Exception("Service not found");
+        }
+
+        public IWorkflowContext GetWfContext(CodeActivityContext context)
+        {
+            return context.GetExtension<IWorkflowContext>(); ;
+        }
+    }
+}
